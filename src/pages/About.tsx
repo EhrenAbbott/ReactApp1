70926e3f2f083dd60cf3f752d5b9b4051c73eb51
Ehren Abbott <era12358@gmail.com>
2023-05-22 12:59:59 -0500

    deleted extra text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,7 +9,6 @@ function About() {
         <div className='flex place-items-center h-screen'>
         </div> 
           <h3 className='place-items-center flex p-9 bg-black bg-opacity-70 text-gray-300 rounded '>Enjoy a new way to manage your inventory!</h3>
-          <h3>blahblahblah</h3>
         <div> 
           
         </div>
